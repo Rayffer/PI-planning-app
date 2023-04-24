75674262be682829cd3e20cdf4828eb6d1fd383e
Rayffer <oscar.guillamon@gmail.com>
2023-04-24 08:08:33 +0200

    add data template selector for element display

--- PiPlanningApp/DataTemplateSelectors/GridElementDataTemplateSelector.cs
+++ PiPlanningApp/DataTemplateSelectors/GridElementDataTemplateSelector.cs
@@ -3,23 +3,22 @@ using System.Windows.Controls;
 
 using PiPlanningApp.Models;
 
-namespace PiPlanningApp.DataTemplateSelectors
-{
-    public class GridElementDataTemplateSelector : DataTemplateSelector
-    {
-        public DataTemplate FeatureTemplate { get; set; }
-        public DataTemplate IterationTemplate { get; set; }
-        public DataTemplate IterationFeatureSlotTemplate { get; set; }
+namespace PiPlanningApp.DataTemplateSelectors;
 
-        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+public class GridElementDataTemplateSelector : DataTemplateSelector
+{
+    public DataTemplate FeatureTemplate { get; set; }
+    public DataTemplate IterationTemplate { get; set; }
+    public DataTemplate IterationFeatureSlotTemplate { get; set; }
+
+    public override DataTemplate SelectTemplate(object item, DependencyObject container)
+    {
+        return item switch
         {
-            return item switch
-            {
-                Feature => this.FeatureTemplate,
-                Iteration => this.IterationTemplate,
-                IterationFeatureSlot=> this.IterationFeatureSlotTemplate,
-                _ => base.SelectTemplate(item, container)
-            };
-        }
+            Feature => this.FeatureTemplate,
+            Iteration => this.IterationTemplate,
+            IterationFeatureSlot => this.IterationFeatureSlotTemplate,
+            _ => base.SelectTemplate(item, container)
+        };
     }
 }
