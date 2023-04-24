46bf03899043e96a71108e2722fade79cf5a2370
Rayffer <oscar.guillamon@gmail.com>
2023-04-24 07:58:17 +0200

    add serialization model

---
+++ PiPlanningApp/Models/ApplicationInformation.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace PiPlanningApp.Models;
+
+public class ApplicationInformation
+{
+    public List<Feature> Features { get; set; } = new List<Feature>();
+    public List<Iteration> Iterations { get; set; } = new List<Iteration>();
+    public List<IterationFeatureSlot> IterationFeatureSlots { get; set; } = new List<IterationFeatureSlot>();
+}
