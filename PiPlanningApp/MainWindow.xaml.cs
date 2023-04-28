1a32180ff70059478dfc1fee198d76c6c2bec74a
Rayffer <oscar.guillamon@gmail.com>
2023-04-28 08:35:20 +0200

    Add panning and zomming functions

--- PiPlanningApp/MainWindow.xaml.cs
+++ PiPlanningApp/MainWindow.xaml.cs
@@ -42,10 +42,10 @@ public partial class MainWindow : Window
 
             previousParentIteration.LoadCapacity =
                 mainWindowViewModel.IterationFeatureSlots.Where(x => x.ParentIterationId == previousParentIteration.Id)
-                                                                                            .Sum(x => x.UserStories.Sum(y => y.StoryPoints));
+                                                         .Sum(x => x.UserStories.Sum(y => y.StoryPoints));
             previousParentFeature.TotalStoryPoints =
                 mainWindowViewModel.IterationFeatureSlots.Where(x => x.ParentFeatureId == previousParentFeature.Id)
-                                                                                              .Sum(x => x.UserStories.Sum(y => y.StoryPoints));
+                                                         .Sum(x => x.UserStories.Sum(y => y.StoryPoints));
 
             mainWindowViewModel.SaveChanges();
 
@@ -74,6 +74,102 @@ public partial class MainWindow : Window
                                                          .Sum(x => x.UserStories.Sum(y => y.StoryPoints));
 
             e.Handled = true;
+        }
+    }
+
+    private void ScrollViewer_MouseWheel(object sender, MouseWheelEventArgs e)
+    {
+        if (Keyboard.Modifiers == ModifierKeys.Control)
+        {
+            if (this.MainGridScaleTransform.ScaleX >= 0.1 &&
+                this.MainGridScaleTransform.ScaleX <= 4 &&
+                this.MainGridScaleTransform.ScaleY >= 0.1 &&
+                this.MainGridScaleTransform.ScaleY <= 4)
+            {
+                this.MainGridScaleTransform.ScaleX += e.Delta / 1200D;
+                this.MainGridScaleTransform.ScaleY += e.Delta / 1200D;
+
+                this.MainGridScaleTransform.ScaleX = this.MainGridScaleTransform.ScaleX switch
+                {
+                    < 0.1 => 0.1,
+                    > 4.0 => 4.0,
+                    _ => this.MainGridScaleTransform.ScaleX
+                };
+                this.MainGridScaleTransform.ScaleY = this.MainGridScaleTransform.ScaleY switch
+                {
+                    < 0.1 => 0.1,
+                    > 4.0 => 4.0,
+                    _ => this.MainGridScaleTransform.ScaleY
+                };
+            }
+        }
+        else
+        {
+            if (Keyboard.Modifiers == ModifierKeys.Shift)
+            {
+                this.MainGridTranslateTransform.X += e.Delta;
+
+                if (this.MainGridTranslateTransform.X < (-(this.MainGrid.ActualWidth - this.ActualWidth) - this.margin))
+                {
+                    this.MainGridTranslateTransform.X = (-(this.MainGrid.ActualWidth - this.ActualWidth) - this.margin);
+                }
+                if (this.MainGridTranslateTransform.X > this.margin)
+                {
+                    this.MainGridTranslateTransform.X = this.margin;
+                }
+            }
+            else
+            {
+                this.MainGridTranslateTransform.Y += e.Delta;
+                if (this.MainGridTranslateTransform.Y < (-(this.MainGrid.ActualHeight - this.ActualHeight) - this.margin))
+                {
+                    this.MainGridTranslateTransform.Y = (-(this.MainGrid.ActualHeight - this.ActualHeight) - this.margin);
+                }
+                if (this.MainGridTranslateTransform.Y > this.margin)
+                {
+                    this.MainGridTranslateTransform.Y = this.margin;
+                }
+            }
+        }
+        e.Handled = true;
+    }
+
+    private Point previousPosition;
+    private double margin = 200;
+
+    private void ScrollViewer_MouseMove(object sender, MouseEventArgs e)
+    {
+        if (Mouse.MiddleButton != MouseButtonState.Pressed)
+        {
+            e.Handled = true;
+            this.previousPosition = default;
+            return;
+        }
+        if (this.previousPosition == default)
+        {
+            this.previousPosition = e.GetPosition(this);
+            e.Handled = true;
+            return;
+        }
+        var currentPosition = e.GetPosition(this);
+        this.MainGridTranslateTransform.X += (currentPosition.X - this.previousPosition.X);
+        this.MainGridTranslateTransform.Y += (currentPosition.Y - this.previousPosition.Y) * 2;
+        this.previousPosition = e.GetPosition(this);
+        if (this.MainGridTranslateTransform.X < (-(this.MainGrid.ActualWidth - this.ActualWidth) - this.margin))
+        {
+            this.MainGridTranslateTransform.X = (-(this.MainGrid.ActualWidth - this.ActualWidth)  - this.margin) ;
+        }
+        if (this.MainGridTranslateTransform.X > this.margin)
+        {
+            this.MainGridTranslateTransform.X = this.margin;
+        }
+        if (this.MainGridTranslateTransform.Y < (-(this.MainGrid.ActualHeight - this.ActualHeight) - this.margin))
+        {
+            this.MainGridTranslateTransform.Y = (-(this.MainGrid.ActualHeight - this.ActualHeight) - this.margin) ;
+        }
+        if (this.MainGridTranslateTransform.Y > this.margin)
+        {
+            this.MainGridTranslateTransform.Y = this.margin;
         }
     }
 }
