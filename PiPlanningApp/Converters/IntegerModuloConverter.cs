bd2a20350bb4a73236d8de77814591725456bea0
Rayffer <oscar.guillamon@gmail.com>
2023-04-28 08:35:10 +0200

    control user story disposition

---
+++ PiPlanningApp/Converters/IntegerModuloConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PiPlanningApp.Converters
+{
+    internal class IntegerModuloConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int integerValue &&
+                parameter is string stringValue &&
+                int.TryParse(stringValue, out var numberToDivideBy))
+            {
+                return integerValue % numberToDivideBy;
+            }
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
